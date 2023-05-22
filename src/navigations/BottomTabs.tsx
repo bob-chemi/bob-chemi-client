d1a777b8dd381f3cee6d3e41e79eae03b77300c3
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-05-22 16:13:33 +0900

    [INIT] React Navigation

---
+++ src/navigations/BottomTabs.tsx
@@ -0,0 +1,25 @@
+import Home from '@/screens/Home';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {View, Text} from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+// 임시 Settings 지워질 것
+const Settings = () => {
+  return (
+    <View>
+      <Text>Setting</Text>
+    </View>
+  );
+};
+
+const BottomTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabs;
---
+++ src/navigations/RootNavigation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BottomTabs from '@/navigations/BottomTabs';
+
+const RootNativeStack = createNativeStackNavigator();
+
+const RootNavigation = () => {
+  return (
+    <RootNativeStack.Navigator screenOptions={{headerShown: false}}>
+      {/** BottomTabs 안에 앱의 하단에서 쓸 Screen들이 존재 */}
+      <RootNativeStack.Screen name="Tab" component={BottomTabs} />
+    </RootNativeStack.Navigator>
+  );
+};
+
+export default RootNavigation;
