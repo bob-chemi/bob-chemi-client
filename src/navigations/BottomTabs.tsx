0d389962a0cc75dd8ad04155e908cd07ce2ae97e
sacultang <sacultang@gmail.com>
2023-05-27 01:00:35 +0900

    chore : RegisterScreen ing..

--- src/navigations/BottomTabs.tsx
+++ src/navigations/BottomTabs.tsx
@@ -1,7 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from '@/screens/home/Home'
+import LoginScreen from '@/screens/login/LoginScreen'
 import PreviewScreen from '@/screens/previewScreen/PreviewScreen'
-import LoginScreen from '@/screens/register/login/LoginScreen'
+import RegisterScreen from '@/screens/register/RegisterScreen'
 
 const Tab = createBottomTabNavigator()
 
@@ -11,6 +12,7 @@ const BottomTabs = () => {
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="PreviewScreen" component={PreviewScreen} />
       <Tab.Screen name="LoginScreen" component={LoginScreen} />
+      <Tab.Screen name="RegisterScreen" component={RegisterScreen} />
     </Tab.Navigator>
   )
 }
