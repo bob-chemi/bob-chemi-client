2657146387e1537c59f4e5ead368971dc7f6aa35
Juhyun Kim <122796509+jhkim34@users.noreply.github.com>
2023-06-06 14:10:47 +0900

    feat, chore: 소모임 생성하기 페이지 추가, react-native-community/datetimepicker, react-native-picker/picker, react-native-image-picker 패키지 추가

--- src/screens/social/PostSocialScreen.tsx
+++ src/screens/social/PostSocialScreen.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native'
 import * as S from './SocialScreen.style'
-import PostedTextComp from './components/PostedTextComp';
+import PostedTextComp from './components/post/PostedTextComp';
+import PostedTimeComp from './components/post/PostedTimeComp';
+import PostedLocationComp from './components/post/PostedLocationComp';
+import PostedImgComp from './components/post/PostedImgComp';
 
 const PostSocialScreen = () => {
   const [image, setImage] = useState('');
@@ -16,10 +19,13 @@ const PostSocialScreen = () => {
         <S.Title>소모임 생성</S.Title>
       </S.BlockTop>
       <View>
-        <PostedTextComp headTitle='제목' bodyContent='hi' height={35} isContent={false}></PostedTextComp>
-        <PostedTextComp headTitle='내용' bodyContent='hi' height={200} isContent={true}></PostedTextComp>
+        <PostedImgComp></PostedImgComp>
+        <PostedLocationComp></PostedLocationComp>
+        <PostedTimeComp></PostedTimeComp>
+        <PostedTextComp headTitle='제목' height={35} isContent={false}></PostedTextComp>
+        <PostedTextComp headTitle='내용' height={300} isContent={true}></PostedTextComp>
       </View>
-      {/* <Button title="등록"></Button> */}
+      <S.PostBtn><S.PostBtnText>등록</S.PostBtnText></S.PostBtn>
     </S.Container>
   )
 }
