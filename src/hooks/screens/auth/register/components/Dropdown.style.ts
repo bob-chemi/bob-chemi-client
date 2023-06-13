fb9c45a780776e36f968092e2d4ac6eab3b6c2c5
sacultang <sacultang@gmail.com>
2023-06-14 00:01:22 +0900

    chore : delete hooks 폴더 내 common 중복 폴더

--- src/hooks/screens/auth/register/components/Dropdown.style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native'
-import theme from '@/common/style/theme'
-
-export const InputLabel = styled.Text`
-  font-size: 13px;
-  color: ${theme.colors.primary};
-  margin-bottom: 10px;
-`
--- src/hooks/screens/home/components/AutoCompleteSearchBar.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native'
-
-export const SearchBarLayout = styled.View`
-  position: absolute;
-  top: 20px;
-  width: 100%;
-  z-index: 20;
-  padding: ${({ theme }) => theme.paddings.default};
-`
