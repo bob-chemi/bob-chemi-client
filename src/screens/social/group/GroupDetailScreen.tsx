8dcc15578251faeda53f2250d6e506001de24467
jhkim <kjuhyun3343@gmail.com>
2023-06-25 13:00:20 +0900

    feat: 소모임 request 페이지 생성, 자신이 만든 소모임 게시글 삭제 기능 추가

--- src/screens/social/group/GroupDetailScreen.tsx
+++ src/screens/social/group/GroupDetailScreen.tsx
@@ -3,7 +3,7 @@ import { CompositeNavigationProp, useNavigation } from '@react-navigation/native
 import { RouteProp } from '@react-navigation/native'
 import { NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack'
 import React, { useEffect } from 'react'
-import { ImageSourcePropType, ScrollView } from 'react-native'
+import { Alert, ImageSourcePropType, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import * as S from '../group/GroupScreen.style'
 import theme from '@/common/style/theme'
@@ -11,6 +11,9 @@ import { TabParamList } from '@/navigations/BottomTabs'
 import { RootNativeStackParamList } from '@/navigations/RootNavigation'
 import { StackParamList } from '@/navigations/StackNav'
 import { SetFormattedDate, SetFormattedTwoDigitNumber } from '@/utils/formattedNum'
+import { useRecoilValue } from 'recoil';
+import { userStatesAtom } from '@/recoil/atoms/userStatesAtom'
+import { groupRequest } from '@/api/groupRequest'
 
 type GroupDetailRouteProp = RouteProp<StackParamList, 'GroupDetailScreen'>
 
@@ -25,7 +28,11 @@ type GroupNavigationProp = CompositeNavigationProp<
 
 const GroupDetailScreen: React.FC<GroupDetailProps> = ({ route }) => {
   const groupData = route.params.group
+  const user = useRecoilValue(userStatesAtom);
+  const isMyPost = groupData.owner.id === user?.user.id ? true : false;
   const navigation = useNavigation<GroupNavigationProp>()
+  const { deleteGroup } = groupRequest
+
   useEffect(() => {
     navigation.setOptions({
       headerTitleAlign: 'center',
@@ -39,14 +46,31 @@ const GroupDetailScreen: React.FC<GroupDetailProps> = ({ route }) => {
 
   const date = SetFormattedDate(groupData.createdAt);
   const imageSource: ImageSourcePropType = groupData.status == 'PUBLIC' ? require('@assets/images/group_public.png') : require('@assets/images/group_private.png');
+
+  const handleDelete = () => {
+    Alert.alert(
+      '삭제 확인',
+      '정말로 삭제하시겠습니까?',
+      [
+        { text: "취소", style: "cancel" },
+        { text: "삭제", style: "destructive", onPress: DeleteGroup }
+      ]
+    )
+  }
+
+  const DeleteGroup = async () => {
+    await deleteGroup(groupData.groupId);
+    navigation.navigate('Tab', { screen: 'Social', params: { tab: 'group' } });
+  }
+
   // <S.GroupDetailImage source={imageSource}></S.GroupDetailImage>*/} {/*[TODO] 수정 필요 2023.06.18 by 김주현
 
-  return ( //[TODO] SubTitleText에 user nickname 추가 필요, img 추가 필요 2023.06.18 by 김주현
+  return ( //[TODO] img 추가 필요 2023.06.18 by 김주현
     <S.Container>
       <S.TitleArea>
         <S.TitleText>{groupData.title}</S.TitleText>
         <S.SubTitleText>
-          {`${date.year}-${SetFormattedTwoDigitNumber(date.month)}-${SetFormattedTwoDigitNumber(date.day)} ${SetFormattedTwoDigitNumber(date.hour)}:${SetFormattedTwoDigitNumber(date.minute)}:${SetFormattedTwoDigitNumber(date.second)}`}
+          {`${date.year}-${SetFormattedTwoDigitNumber(date.month)}-${SetFormattedTwoDigitNumber(date.day)} ${SetFormattedTwoDigitNumber(date.hour)}:${SetFormattedTwoDigitNumber(date.minute)}:${SetFormattedTwoDigitNumber(date.second)} | ${groupData.owner.nickname}`}
         </S.SubTitleText>
       </S.TitleArea>
       <S.GroupDetailArea>
@@ -64,9 +88,22 @@ const GroupDetailScreen: React.FC<GroupDetailProps> = ({ route }) => {
             <S.ContextText>{groupData.description}</S.ContextText>
           </ScrollView>
         </S.ContextArea>
-        <S.Btn style={{ alignSelf: 'center' }}>
-          <S.BtnText>참여하기</S.BtnText>
-        </S.Btn>
+        {isMyPost ? (
+          <S.BetweenBtnArea>
+            <S.BetweenBtn>
+              <S.BtnText>수정하기</S.BtnText>
+            </S.BetweenBtn>
+            <S.BetweenBtn onPress={handleDelete}>
+              <S.BtnText>삭제하기</S.BtnText>
+            </S.BetweenBtn>
+          </S.BetweenBtnArea>
+        ) :
+          (
+            <S.Btn style={{ alignSelf: 'center' }}>
+              <S.BtnText>참여하기</S.BtnText>
+            </S.Btn>
+          )
+        }
       </S.GroupDetailArea>
     </S.Container>
   )
--- src/screens/social/group/GroupScreen.tsx
+++ src/screens/social/group/GroupScreen.tsx
@@ -9,21 +9,22 @@ import theme from '@/common/style/theme'
 import { Nav } from '@/types/nav'
 import { groupRequest } from '@/api/groupRequest'
 import { Group } from '@/types/socialType'
-const { getGroupByDate } = groupRequest
-const renderContentsItem = ({ item }: { item: Group }) => {
-  return (
-    <ContentsBox GroupData={item}></ContentsBox>
-  )
-}
 
 const GroupScreen = () => {
   const { navigate } = useNavigation<Nav>()
+  const { getGroupByDate } = groupRequest
   const [contentsData, setContentsData] = useState<Group[]>([]);
   const [selectedDate, setSelectedDate] = useState(new Date().getDate().toString());
 
   const handleDateChange = (newDate: string) => {
     setSelectedDate(newDate);
     getList(newDate);
+  }
+
+  const renderContentsItem = ({ item }: { item: Group }) => {
+    return (
+      <ContentsBox GroupData={item}></ContentsBox>
+    )
   }
 
   const getList = async (getDate: string) => { //소모임 리스트 GET 요청
@@ -45,8 +46,16 @@ const GroupScreen = () => {
   const NavToCreate = () => {
     navigate('PostGroupScreen');
   }
+
+  const NavToJoinList = () => {
+    navigate('JoinListScreen');
+  }
+
   return (
     <S.Container>
+      <S.JoinBtn onPress={(NavToJoinList)}>
+        <Icon name="heart" color={theme.colors.success} size={24} />
+      </S.JoinBtn>
       <S.WeekContainer>
         <WeekDayPicker selectedDate={selectedDate} onChangeDate={handleDateChange}></WeekDayPicker>
       </S.WeekContainer>
@@ -58,7 +67,7 @@ const GroupScreen = () => {
           columnWrapperStyle={{ justifyContent: 'space-between' }}>
         </FlatList>
       </S.ContentsContainer>
-      <S.CreateButton onPress={NavToCreate}><Icon name="pencil" color={theme.colors.white} size={30}></Icon></S.CreateButton>
+      <S.CreateButton onPress={((NavToCreate))}><Icon name="pencil" color={theme.colors.white} size={30}></Icon></S.CreateButton>
     </S.Container>
   )
 }
